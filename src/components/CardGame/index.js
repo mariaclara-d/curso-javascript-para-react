e30076094cb0d2956c163911bf32d7de7c4c9abb
Maria Clara Guimarães <78209031+mariaclara-d@users.noreply.github.com>
2022-04-02 12:14:42 -0300

    criando repositorio do projeto

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import aluraPixel from '../../../images/alura-pixel.png';
+
+function CardGame() {
+
+    return `
+    <article class="card-game">
+    <img src= ${aluraPixel} />
+    </article>
+    `;
+}
+
+export default CardGame;
