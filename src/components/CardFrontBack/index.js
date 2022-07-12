6264d84e4d6a341468a10b1fdd90bd2701a879fb
Maria Clara Guimarães <78209031+mariaclara-d@users.noreply.github.com>
2022-07-12 14:44:17 -0300

    CardFrontBack

---
+++ src/components/CardFrontBack/index.js
@@ -0,0 +1,15 @@
+import CardGame from "../CardGame";
+
+function CardFrontBack (){
+
+    return  /*html*/ `
+    <article class="card-front-back">
+    ${CardGame()}
+    ${CardGame()}
+    </article>
+
+    `;
+
+}
+
+export default CardFrontBack;
